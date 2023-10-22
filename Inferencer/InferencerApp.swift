3def3f0644f5b0ea68af400775feecc221f6e207
sagor155670 <sagor@braincraftapps.com>
2023-10-22 19:45:53 +0600

    Initial Commit

---
+++ Inferencer/InferencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InferencerApp.swift
+//  Inferencer
+//
+//  Created by Saiful Islam Sagor on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InferencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
